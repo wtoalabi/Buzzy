1595306e86744f8fc69b0e8aa84ff248f59a2763
Wale <wtoalabi@gmail.com>
2019-01-04 15:39:59 +0100

    -C

--- app/User.php
+++ app/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
   public function avatarUrl(){
     $userID = $this->id;
     if(str_contains($this->avatar, 'https://') || str_contains($this->avatar, 'http://')){
-      return $this->avatar;we
+      return $this->avatar;
     }
     return "storage/avatar/$userID/$this->avatar";
   }
