730aea821e0bba6716ac5567127270e6026caeb1
Wale <wtoalabi@gmail.com>
2019-01-05 14:19:36 +0100

    -C

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,8 @@
       $this->call(UserSeeder::class);
       $this->call(TagSeeder::class);
       $this->call(WordSeeder::class);
+      $this->call(WordTagsSeeder::class);
       //$this->call(AudioSeeder::class);
-      //$this->call(WordTagsSeeder::class);
       //$this->call(DescriptionSeeder::class);
       //$this->call(DescriptionAudioSeeder::class);
       //$this->call(SocialProfileSeeder::class);
